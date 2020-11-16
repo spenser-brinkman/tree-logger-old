ef407710ce83ba50e2ef4b0ac22e2bad21756ee0
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-16 15:48:22 -0800

    species selection logic

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,3 +1,9 @@
 class TreesController < ApplicationController
-  
+  post '/trees' do
+    if params[:tree][:species_id] == "" && params[:new_species]
+      # code
+    else
+      # code
+    end
+  end
 end
