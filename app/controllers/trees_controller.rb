ae89c8dcfc57a304b9b11020dae85dc7b1c4c5c8
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-17 14:28:53 -0800

    able to post new tree

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,17 +1,27 @@
 class TreesController < ApplicationController
   
+  get '/trees' do
+    erb :"trees/index"
+  end
+
   get '/trees/new' do
     erb :"trees/new"
   end
-  
+
   post '/trees' do
-    binding.pry
-    if params[:tree][:species_id] == "" && params[:new_species] != ""
-    #   # code
-    elsif params[:tree][:species_id] == "" && params[:new_species] == ""
-        # code for if no species selected
-    else
-    #   # code
+    if params[:tree][:species_id] == "" && params[:new_species] != ""                      # If user wants to enter new species
+      species = Species.create(name: params[:new_species])
+    elsif params[:tree][:species_id] == "" && params[:new_species] == ""                   # If user does not select a species
+      species = Species.find_by_name("Unknown")
     end
+    tree = current_user.trees.build(params[:tree])
+    tree.species_id = species.id
+    tree.save
+    redirect to "/trees/#{tree.id}"
+  end
+
+  get '/trees/:id' do
+    @tree = current_user.trees.find(params[:id])
+    erb :"trees/view"
   end
 end
