432c4defa1cb2c788a40460f233e87519a100a9a
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-17 09:53:31 -0800

    fleshed out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,42 @@
 class UsersController < ApplicationController
-  
+
+  get '/signup' do
+    erb :"users/signup"
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if !@user.save
+      erb :"users/signup"
+    elsif @user.save
+      redirect to "/login"
+    end
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/home"
+    else
+      @err = "Invalid Credentials"
+      erb :"users/login"
+    end
+  end
+
+  get '/home' do
+    @user = current_user
+    erb :"users/home"
+  end
+
+  get '/logout' do
+    session.destroy
+    redirect to "/"
+  end
+
+
 end
