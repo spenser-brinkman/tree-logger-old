12341eccff7e4453610816c0bf516dd54db29625
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-16 16:00:15 -0800

    add /trees/new controller

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,4 +1,9 @@
 class TreesController < ApplicationController
+  
+  get '/trees/new' do
+    erb :"trees/new"
+  end
+  
   post '/trees' do
     if params[:tree][:species_id] == "" && params[:new_species]
       # code
