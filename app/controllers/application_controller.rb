a8fe57fb0efc1dc9297889ab7358fa8115f28add
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-17 09:53:16 -0800

    session secret and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, '66f1e7b1c6ad78e94a063deaf705160f'
   end
 
   get "/" do
     erb :landing
   end
 
+  
+  helpers do
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def authentication
+      redirect '/login' if !logged_in?
+    end
+
+    def authorize(resource)
+      redirect back if resource.user != current_user
+    end
+  
+  end
 end
