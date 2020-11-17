2243a5c5b4cfddcc16b3e1ea2ef13953b0c4a326
spenser-brinkman <brinkman.spenser@gmail.com>
2020-11-17 15:33:40 -0800

    add authentication

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,27 +1,51 @@
 class TreesController < ApplicationController
   
   get '/trees' do
-    erb :"trees/index"
+    if !logged_in?
+      redirect to "/login"
+    else
+      erb :"trees/index"
+    end
   end
 
   get '/trees/new' do
-    erb :"trees/new"
+    if !logged_in?
+      redirect to "/login"
+    else
+      erb :"trees/new"
+    end
   end
 
   post '/trees' do
-    if params[:tree][:species_id] == "" && params[:new_species] != ""                      # If user wants to enter new species
-      species = Species.create(name: params[:new_species])
-    elsif params[:tree][:species_id] == "" && params[:new_species] == ""                   # If user does not select a species
-      species = Species.find_by_name("Unknown")
+    if !logged_in?
+      redirect to "/login"
+    else
+      if params[:tree][:species_id] == "" && params[:new_species] != ""                      # If user wants to enter new species
+        species = Species.create(name: params[:new_species])
+      elsif params[:tree][:species_id] == "" && params[:new_species] == ""                   # If user does not select a species
+        species = Species.find_by_name("Unknown")
+      end
+      tree = current_user.trees.build(params[:tree])
+      tree.species_id = species.id
+      tree.save
+      redirect to "/trees/#{tree.id}"
     end
-    tree = current_user.trees.build(params[:tree])
-    tree.species_id = species.id
-    tree.save
-    redirect to "/trees/#{tree.id}"
   end
 
   get '/trees/:id' do
-    @tree = current_user.trees.find(params[:id])
-    erb :"trees/view"
+    if !logged_in?
+      redirect to "/login"
+    else
+      @tree = current_user.trees.find(params[:id])
+      erb :"trees/view"
+    end
+  end
+
+  get '/trees/:id/edit' do
+    if !logged_in?
+      redirect to "/login"
+    else
+
+    end
   end
 end
